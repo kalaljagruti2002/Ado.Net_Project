2c1ea611c8c425ff080fdf7d6b8e5bfb5c50678d
Jagruti Kalal <jagruti.kalal1@gmail.com>
2024-11-08 15:48:02 +0600

    Add files via upload

---
+++ Controllers/EmpolyeeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ADO.Net_Project.Models;
+using ADO.Net_Project.Repository;
+
+namespace ADO.Net_Project.Controllers
+{
+    public class EmpolyeeController : Controller
+    {
+       EmpRepository repo = new EmpRepository();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult save(EmpoyeeModel model)
+        {
+            if (model != null)
+            {
+                bool result = repo.AddEmployee(model);
+                TempData["msg"] = "Save Data Successfully...!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+				TempData["msg"] = "Save Data Empty...!";
+				return RedirectToAction("Index");
+			}
+        }
+    }
+}
---
+++ Models/EmpoyeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADO.Net_Project.Models
+{
+	public class EmpoyeeModel
+	{
+		public int Emp_id { get; set; }	
+
+		public String Emp_Name { get; set; }
+
+		public string Address { get; set; }
+
+		public string Email { get; set; }
+
+		public string Salary { get; set; }
+	}
+}
